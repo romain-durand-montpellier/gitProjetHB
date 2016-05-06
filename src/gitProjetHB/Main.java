5795e6e5889b58741f2b2ffa63b1849399edb293
SebastienGrana <granarolo.sebastien@gmail.com>
2016-05-06 11:26:30 +0200

    Commit 3

--- src/gitProjetHB/Main.java
+++ src/gitProjetHB/Main.java
@@ -4,29 +4,60 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		int newNumber=0;
-		while (newNumber != -1) {
-			newNumber = inputNumber();	
-		}
+		
+		
+		
+			biggerNumber();	
+		
 		
 
 	}
-	private static int inputNumber() {//Return a number typed by the user
-		Integer number = null;
+	
+	
+	private static int biggerNumber() {//Return a number typed by the user
 		
+		//init
+		int number1 = -2;
+		int number2 = -2;
+		int number3 = -2;
+		int number = -2;
+		int minNumber = -2;
 		Scanner reader = new Scanner(System.in);
-		System.out.println ("Entrez un nombre: ");
+		
 
-		while (number==null)
+		do //turn until -1 is enter
 		{
-		try {
+
+			minNumber = Math.min(number3, Math.min(number1, number2));
+			
+		try { 
+			System.out.println ("Entrez un nombre: ");
 			number = reader.nextInt();
+			
+			
+			if(number > minNumber){
+				number1 = number;
+			}
+			
+			if(number1 > 0){ //if number > 0 print it
+				System.out.println (number1);
+			}
+			if(number2 > 0){
+				System.out.println (number2);
+			}
+			if(number3 > 0){
+				System.out.println (number3);
+			}
+			
+			
 		  }
 		catch (java.util.InputMismatchException e) {
 			reader.nextLine();
-		    System.out.println("Entrée non valide !");
+		    System.out.println("Entrï¿½e non valide !");
 		  }
-		}
+		
+		}while (number != -1);
+		
 		return (number);
 	}
 }
