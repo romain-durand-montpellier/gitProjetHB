9cae537229e38573ea7af41d492b58091fd56c3a
Romain D <romain-durand@laposte.net>
2016-05-06 10:29:45 +0200

    Ajout fonction saisie du nombre

--- src/gitProjetHB/Main.java
+++ src/gitProjetHB/Main.java
@@ -1,5 +1,32 @@
 package gitProjetHB;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		int newNumber=0;
+		while (newNumber != -1) {
+			newNumber = inputNumber();	
+		}
+		
+
+	}
+	private static int inputNumber() {//Return a number typed by the user
+		Integer number = null;
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.println ("Entrez un nombre: ");
+
+		while (number==null)
+		{
+		try {
+			number = reader.nextInt();
+		  }
+		catch (java.util.InputMismatchException e) {
+			reader.nextLine();
+		    System.out.println("Entrée non valide !");
+		  }
+		}
+		return (number);
+	}
 }
